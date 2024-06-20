61966094764320d0fef806f1763a7401c532bf84
CaioXyZ <caioreis29974@gmail.com>
2024-06-20 00:50:53 -0300

    arrumando código

--- exercícios/exe005.py
+++ exercícios/exe005.py
@@ -1,4 +1,4 @@
 n = int(input('Digite um número: '))
 s = n + 1
 a = n - 1
-print('O sucessor de', n, 'é', s, 'e o antecessor é', a)
+print('O sucessor de {} é {} e o antecessor é {}.'.format(n, n+1, n-1))
